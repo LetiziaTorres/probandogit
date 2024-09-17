7a641970b4ab7cae1c42c9623fab945eb573cda5
LetiziaTorres <letizia.torres@utec.edu.pe>
2024-09-16 19:29:58 -0500

    Nuevo par de claves

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Primer proyecto con git
 - Primer cambio: Crear un archivo 'main.py'
 - Segundo cambio: Añadir linea al archivo 'main.py'
+- Tercer cambio: Par de claves SSH creadas
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("este es mi primer proyecto con git")
 print("Otro cambio para el tercer commit de mi repositorio")
+print("Ya tengo mi par de claves SSH")
 print ("Hello wordl")
